b229d4535090f56120371c257d6648933d9b1151
David <david.bismut@gmail.com>
2020-11-11 00:51:00 +0100

    fixes button in data tree

--- src/helpers/button.ts
+++ src/helpers/button.ts
@@ -2,5 +2,5 @@ import { InputTypes } from './../types'
 
 export function button(name: string, fn: () => any) {
   console.log('button', { name })
-  return { __type: InputTypes.BUTTON, name, fn }
+  return { [name]: { __type: InputTypes.BUTTON, fn } }
 }
