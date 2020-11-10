904b6da58fa5514a6e54f45ed305c9fcb3cc5d7a
David <david.bismut@gmail.com>
2020-11-10 23:46:50 +0100

    ts: assert getKeyPath always returns a key

--- src/utils.ts
+++ src/utils.ts
@@ -8,9 +8,9 @@ export const join = (...args: (string | undefined)[]) => args.filter(Boolean).jo
 export const prefix = (obj: object, p: string) =>
   Object.entries(obj).reduce((acc, [key, v]) => ({ ...acc, [join(p, key)]: v }), {})
 
-export function getKeyPath(path: string) {
+export function getKeyPath(path: string): [string, string | undefined] {
   const dir = path.split('.')
-  return [dir.pop(), dir.join('.') || undefined]
+  return [dir.pop()!, dir.join('.') || undefined]
 }
 
 export function pick<K extends string, T extends { [k in K]: unknown }>(object: T, keys: K[]) {
