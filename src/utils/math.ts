5bc5842fce05d0c15bb1f85383a4762d45a89e21
David <david.bismut@gmail.com>
2020-11-27 23:33:50 +0100

    shorter in minification

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,4 +1,4 @@
-export const clamp = (x: number, min: number, max: number) => Math.max(min, Math.min(max, x))
+export const clamp = (x: number, min: number, max: number) => (x > max ? max : x < min ? min : x)
 export const floor = (min: number, x: number) => Math.max(min, Math.floor(x))
 export const pad = (x: number, pad: number) => String(x).padStart(pad, '0')
 
