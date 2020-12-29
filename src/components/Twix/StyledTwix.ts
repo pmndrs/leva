127aaf7aef804ec3edcbaba6f99f91e7f09d60c1
David <david.bismut@gmail.com>
2020-12-29 10:13:09 +0100

    puts the pane on top

--- src/components/Twix/StyledTwix.ts
+++ src/components/Twix/StyledTwix.ts
@@ -11,6 +11,7 @@ export const Root = styled.div<{ fillParent: boolean }>`
           right: 10px;
           color: root-text;
           width: root-width;
+          z-index: 1000;
         `
       : css`
           position: relative;
