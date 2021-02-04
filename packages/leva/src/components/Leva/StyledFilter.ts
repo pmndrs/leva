e9d7a5f84cdce7ecb4fea021a000faace9691945
David <david.bismut@gmail.com>
2021-02-04 16:05:22 +0100

    Add clear filter button

--- packages/leva/src/components/Leva/StyledFilter.ts
+++ packages/leva/src/components/Leva/StyledFilter.ts
@@ -1,47 +1,6 @@
 import { styled } from '../../styles'
 import { StyledTitle } from '../Folder/StyledFolder'
 
-export const DragHandle = styled(StyledTitle, {
-  position: 'absolute',
-  width: '100%',
-  opacity: 0,
-  touchAction: 'none',
-})
-
-export const StyledTitleWithFilter = styled('div', {
-  display: 'flex',
-  alignItems: 'stretch',
-  justifyContent: 'space-between',
-  height: '43px',
-  cursor: 'grab',
-})
-
-export const FilterWrapper = styled('div', {
-  position: 'relative',
-  width: '100%',
-  overflow: 'hidden',
-  transition: 'height 250ms ease',
-  variants: {
-    toggled: { true: { height: 30 }, false: { height: 0 } },
-  },
-})
-
-export const StyledFilterInput = styled('input', {
-  $reset: '',
-  position: 'relative',
-  height: 30,
-  width: '100%',
-  padding: '0 $md',
-  backgroundColor: 'transparent',
-  color: '$highlight3',
-  fontSize: '10px',
-  borderRadius: '$root',
-  '&:focus': {},
-  '::placeholder': {
-    color: '$highlight2',
-  },
-})
-
 export const Icon = styled('i', {
   $flexCenter: '',
   width: 40,
@@ -55,6 +14,47 @@ export const Icon = styled('i', {
     fill: '$highlight3',
   },
   variants: { active: { true: { '> svg': { fill: '$highlight2' } } } },
+})
+
+export const StyledTitleWithFilter = styled('div', {
+  display: 'flex',
+  alignItems: 'stretch',
+  justifyContent: 'space-between',
+  height: '43px',
+  cursor: 'grab',
+})
+
+export const FilterWrapper = styled('div', {
+  $flex: '',
+  position: 'relative',
+  width: '100%',
+  overflow: 'hidden',
+  transition: 'height 250ms ease',
+  color: '$highlight3',
+  paddingLeft: '$md',
+
+  [`> ${Icon}`]: {
+    height: 30,
+  },
+
+  variants: {
+    toggled: { true: { height: 30 }, false: { height: 0 } },
+  },
+})
+
+export const StyledFilterInput = styled('input', {
+  $reset: '',
+  flex: 1,
+  position: 'relative',
+  height: 30,
+  width: '100%',
+  backgroundColor: 'transparent',
+  fontSize: '10px',
+  borderRadius: '$root',
+  '&:focus': {},
+  '::placeholder': {
+    color: '$highlight2',
+  },
 })
 
 export const Drag = styled('div', {
