341a3fd1ebc3e70f7386e899219a43631a306813
Gianmarco Simone <gsimone@hey.com>
2021-02-13 19:15:50 +0100

    docs: dark mode storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,5 +3,12 @@ export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   "previewTabs": {
     'storybook/docs/panel': { hidden: true }
-  }
+  },
+  options: {
+    storySort: {
+      order: ["Inputs", ["String", "Boolean", "Number", "Interval"], "Misc", "Plugins"]
+    }
+  },
 }
+
+export const  decorators = [(Story) => <div style={{ color: "white" }}><Story /></div>]
