e6db0298cc35f6221a47d5d1a6e20c64c1b70f36
Gianmarco Simone <gsimone@hey.com>
2021-02-13 17:44:36 +0100

    docs: adds stories for alpha colors

--- packages/leva/stories/Color.stories.tsx
+++ packages/leva/stories/Color.stories.tsx
@@ -18,8 +18,16 @@ const Template: Story<any> = (args) => {
     const { color } = values
 
     if (typeof color !== "string") {
-      let c = color as { r: number, g: number, b: number  }
+
+      
+      let c = color as { r: number, g: number, b: number, a?: number  }
+
+      if (c.a) {
+        return `rgb(${c.r}, ${c.g}, ${c.b}, ${c.a})`
+      }
+
       return `rgb(${c.r}, ${c.g}, ${c.b})`
+      
     }
     
     return color
@@ -34,9 +42,19 @@ Hexadecimal.args = {
   value: "#ff005b",
 };
 
+export const Hex8 = Template.bind({});
+Hex8.args = {
+  value: "#ff005b88",
+};
+
 export const RGBObject = Template.bind({});
 RGBObject.args = {
   value: { r: 248, g: 214, b: 40 }
+};
+
+export const RGBAObject = Template.bind({});
+RGBAObject.args = {
+  value: { r: 248, g: 214, b: 40, a: 1 }
 };
 
 export const String = Template.bind({});
