7d5e40737797304289b815cbe5e7fa8c4880eea5
David <david.bismut@gmail.com>
2021-02-14 01:43:07 +0100

    update app to show corrected bugs

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -32,31 +32,31 @@ function Comp1() {
     folder5: folder({
       boolean2: false,
     }),
-    firstsuperlonglabel: { value: '40px', min: 30, max: 90 },
+    firstsuperlonglabel: { value: 10, step: 0.25 },
     myPlugin: greenOrBlue({ color: 'green', light: true, alpha: 0.5 }),
     // wrong: { something: 'else' },
     image: { image: undefined },
     select: { options: ['x', 'y', ['x', 'y']] },
-    interval: { min: -100, max: 100, value: [-10, 10] },
+    interval: { min: -100, max: 100, value: [10, 15] },
     boolean: true,
-    color: { value: '#ffffffff', render: (get) => get('boolean') },
     refMonitor: monitor(ref, { graph: true, interval: 30 }),
     // number: { value: 1000, min: 3 },
     folder2: folder(
       {
-        spring: spring({ tension: 100, friction: 30 }),
+        color: { value: '#ff005b', render: (get) => get('boolean') },
+        spring: spring(),
         folder3: folder(
           {
             'Hello Button': button(() => console.log('hello')),
             folder4: folder({
               string: 'hello',
-              pos2d: { value: { x: 3, y: 4 }, step: 3 },
+              pos2d: { value: { x: 3, y: 4 } },
               pos2dArr: [100, 200],
               pos3d: { x: 0.3, y: 0.1, z: 0.5 },
               pos3dArr: [Math.PI / 2, 20, 4],
             }),
           },
-          { collapsed: false }
+          { collapsed: true }
         ),
       },
       { render: (get) => get('boolean') }
@@ -67,7 +67,7 @@ function Comp1() {
 
   // console.log(t.colorObj)
   // console.log(t.pos2d)
-  // console.log(t.string)
+  // console.log(t.spring)
   // console.log(t.myPlugin)
 
   return (
@@ -117,7 +117,7 @@ export function App1() {
     <>
       <Leva theme={{ colors: { accent1: 'red' } }} oneLineLabels={oneLineLabels} hideTitleBar={hideTitleBar} />
       <div style={{ display: 'flex' }}>
-        <div style={{ width: '50%' }}>{c2 && <Scene3D />}</div>
+        {/* <div style={{ width: '50%' }}>{c2 && <Scene3D />}</div> */}
         <div>
           {c1 && <Comp1 />}
           {c2 && <Comp1 />}
