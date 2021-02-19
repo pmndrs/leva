7ded1023e213d6987bd419518aa5ef5c6ded96cb
David <david.bismut@gmail.com>
2021-02-19 22:24:18 +0100

    manually resolve packages from other repos so that vercel builds properly

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,18 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../packages/**/*.stories.mdx",
-    "../packages/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  stories: ['../packages/**/*.stories.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // manually resolve packages from other repos so that vercel builds properly
+    if (configType === 'PRODUCTION') {
+      config.resolve.alias['leva'] = path.resolve(__dirname, '../packages/leva/')
+    }
+    // Return the altered config
+    return config
+  },
 }
