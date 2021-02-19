8091438c992a2eed465beff9128173c1bc280a34
David <david.bismut@gmail.com>
2021-02-19 16:47:21 +0100

    stories: adds render option

---
+++ packages/leva/stories/Options.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Reset from './components/decorator-reset'
+import { Story, Meta } from '@storybook/react'
+
+import { folder, useControls } from '../src'
+
+export default {
+  title: 'Options',
+  decorators: [Reset],
+} as Meta
+
+const Template: Story<any> = () => {
+  const values = useControls({
+    show: { value: true, label: 'Show color' },
+    color: { value: '#fff', render: (get) => get('show') },
+    show2: { value: false, label: 'Show folder' },
+    folder: folder(
+      {
+        number: 1,
+        string: {
+          value: 'shown if number >=1',
+          render: (get) => get('folder.number') >= 1,
+        },
+      },
+      { render: (get) => get('show2') }
+    ),
+  })
+
+  return (
+    <div>
+      <pre>{JSON.stringify(values, null, '  ')}</pre>
+    </div>
+  )
+}
+
+export const Render = Template.bind({})
