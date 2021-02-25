5e99742f6754414ecc8f1bfa713ac00e31a40cec
David <david.bismut@gmail.com>
2021-02-26 00:16:12 +0100

    chore: style improvements

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -6,22 +6,43 @@ const links = ['leva-minimal', 'leva-plugin-spring', 'leva-busy', 'leva-advanced
 const Example = ({ link }) => {
   const Lazy = React.lazy(() => import(/* @vite-ignore */ `./sandboxes/${link}/src/App`))
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <Lazy />
-    </Suspense>
+    <div>
+      <Link href="/">
+        {/*eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a
+          style={{
+            position: 'absolute',
+            left: 10,
+            top: 10,
+            zIndex: 100,
+            padding: 10,
+            background: '#000',
+            color: '#fff',
+          }}>
+          Back
+        </a>
+      </Link>
+      <Suspense fallback={<div>loading...</div>}>
+        <Lazy />
+      </Suspense>
+    </div>
   )
 }
 
 export default function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      {links.map((l) => (
-        <Link key={l} href={`/${l}`}>
-          {/*eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="link">{l}</a>
-        </Link>
-      ))}
+    <>
+      <Route path="/">
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          {links.map((l) => (
+            <Link key={l} href={`/${l}`}>
+              {/*eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a>{l}</a>
+            </Link>
+          ))}
+        </div>
+      </Route>
       <Route path="/:link">{(params) => <Example link={params.link} />}</Route>
-    </div>
+    </>
   )
 }
