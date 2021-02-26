25121dc27030b205358ab1ec8e1e1d0e451d301a
David <david.bismut@gmail.com>
2021-02-26 17:32:26 +0100

    refactor: clearer var names

--- packages/leva/src/hooks/useVisiblePaths.ts
+++ packages/leva/src/hooks/useVisiblePaths.ts
@@ -6,13 +6,13 @@ import shallow from 'zustand/shallow'
  * Hook used by the root component to get all visible inputs.
  */
 export const useVisiblePaths = (store: StoreType) => {
-  const [state, setState] = useState(store.getVisiblePaths())
+  const [paths, setPaths] = useState(store.getVisiblePaths())
 
   useEffect(() => {
-    setState(store.getVisiblePaths())
-    const unsub = store.useStore.subscribe(setState, store.getVisiblePaths, shallow)
+    setPaths(store.getVisiblePaths())
+    const unsub = store.useStore.subscribe(setPaths, store.getVisiblePaths, shallow)
     return () => unsub()
   }, [store])
 
-  return state
+  return paths
 }
