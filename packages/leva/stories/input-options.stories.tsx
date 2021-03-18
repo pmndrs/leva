505bf11fa29a73cace93c26e33d9c1ac0f2ea203
David Bismut <david.bismut@gmail.com>
2021-03-18 16:44:33 +0100

    story: add story

--- packages/leva/stories/input-options.stories.tsx
+++ packages/leva/stories/input-options.stories.tsx
@@ -73,3 +73,48 @@ export const Optional = () => {
     </div>
   )
 }
+
+function A() {
+  const renderRef = React.useRef(0)
+  const divRef = React.useRef(null)
+  renderRef.current++
+  const data = useControls({
+    color: {
+      value: '#f00',
+      onChange: (v) => {
+        divRef.current.style.color = v
+        divRef.current.innerText = `Transient color is ${v}`
+      },
+    },
+  })
+  return (
+    <div style={{ padding: 20, margin: 20, border: '1px solid black' }}>
+      <pre>A data (should be empty)</pre>
+      <pre>{JSON.stringify(data, null, '  ')}</pre>A rendered {renderRef.current} time
+      <div style={{ marginTop: 20 }} ref={divRef} />
+    </div>
+  )
+}
+
+function B() {
+  const data = useControls({
+    color: { value: '#f00' },
+  })
+  return (
+    <div style={{ padding: 20, margin: 20, border: '1px solid black' }}>
+      <pre>B data (should update)</pre>
+      <pre>{JSON.stringify(data, null, '  ')}</pre>
+    </div>
+  )
+}
+
+export const OnChange = () => {
+  const [showA, setShowA] = React.useState(true)
+  return (
+    <>
+      <button onClick={() => setShowA((s) => !s)}>{showA ? 'Hide A' : 'Show A'}</button>
+      {showA && <A />}
+      <B />
+    </>
+  )
+}
