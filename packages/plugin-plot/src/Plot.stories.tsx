dc2da824dd87cd05405c8738fdfa2547527bdf70
David Bismut <david.bismut@gmail.com>
2021-03-20 18:55:48 +0100

    stories: update

--- packages/plugin-plot/src/Plot.stories.tsx
+++ packages/plugin-plot/src/Plot.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Meta } from '@storybook/react'
+import { Story, Meta } from '@storybook/react'
 
 import Reset from '../../leva/stories/components/decorator-reset'
 import { useControls } from '../../leva/src'
@@ -11,12 +11,57 @@ export default {
   decorators: [Reset],
 } as Meta
 
-export const Plot = () => {
-  const values = useControls({ num: 5, curve: plot({ expression: 'cos(x)', boundsX: [-10, 10], boundsY: [-1, 1] }) })
-
+const Template: Story<any> = (args) => {
+  const { curve } = useControls({ curve: plot(args) })
+  const expr = curve.compile()
   return (
     <div>
-      <pre>{JSON.stringify(values, null, '  ')}</pre>
+      {[0, 0.5, -1].map((x) => (
+        <pre key={x}>
+          x = {x}, curve: {expr.evaluate({ x }).toFixed(2)}
+        </pre>
+      ))}
+    </div>
+  )
+}
+
+export const DefaultBounds = Template.bind({})
+DefaultBounds.args = { expression: 'x' }
+
+export const BoundsX = Template.bind({})
+BoundsX.args = { expression: 'cos(x)', boundsX: [-10, 10] }
+
+export const BoundsY = Template.bind({})
+BoundsY.args = { expression: 'sin(x) * tan(x)', boundsX: [-10, 10], boundsY: [-1, 1] }
+
+export const InputAsVariable = () => {
+  const { curve } = useControls({ var: 10, curve: plot({ expression: 'cos(x * var)' }) })
+  const expr = curve.compile()
+  return (
+    <div>
+      {[0, 0.5, -1].map((x) => (
+        <pre key={x}>
+          x = {x}, curve: {expr.evaluate({ x }).toFixed(2)}
+        </pre>
+      ))}
+    </div>
+  )
+}
+
+export const CurveAsVariable = () => {
+  const { curve } = useControls({
+    var: 10,
+    y1: plot({ expression: 'cos(x * var)' }),
+    curve: plot({ expression: 'x * y1' }),
+  })
+  const expr = curve.compile()
+  return (
+    <div>
+      {[0, 0.5, -1].map((x) => (
+        <pre key={x}>
+          x = {x}, curve: {expr.evaluate({ x }).toFixed(2)}
+        </pre>
+      ))}
     </div>
   )
 }
