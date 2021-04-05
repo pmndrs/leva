41e0a1431e7138e3e227c8e8ee11ac8e50eef807
hasparus <hasparus@gmail.com>
2021-04-05 13:37:41 +0200

    docs: copy circle drag codesandbox into storybook

--- packages/leva/stories/controlled-inputs.stories.tsx
+++ packages/leva/stories/controlled-inputs.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Meta } from '@storybook/react'
+import { ComponentPropsWithoutRef, forwardRef, useRef, MouseEventHandler } from 'react'
+import { Meta, Story } from '@storybook/react'
 import Reset from './components/decorator-reset'
 
 import { useControls } from '../src'
@@ -11,7 +12,7 @@ export default {
 
 const formStyles: React.CSSProperties = { display: 'flex', flexDirection: 'column', gap: '0.5em' }
 
-export const ExternalUpdatesWithSet = () => {
+export const ExternalUpdatesWithSet: Story = () => {
   const [{ username, counter }, set] = useControls(() => ({ username: 'Mario', counter: 0 }))
 
   return (
@@ -28,3 +29,74 @@ export const ExternalUpdatesWithSet = () => {
     </form>
   )
 }
+
+const Circle = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<'div'>>((props, ref) => {
+  return (
+    <div
+      ref={ref}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100px',
+        height: '100px',
+        borderRadius: '50%',
+        backgroundColor: '#eee',
+        border: '1px solid #222',
+      }}
+      {...props}
+    />
+  )
+})
+
+ExternalUpdatesWithSet.storyName = 'external updates with set'
+
+export const OnChangeAndSet: Story = () => {
+  const isDragging = useRef(false)
+  const circleRef = useRef<HTMLDivElement>(null)
+  const [, set] = useControls(() => ({
+    mousePosition: {
+      value: { x: 100, y: 150 },
+      onChange: (position) => {
+        const element = circleRef.current
+        if (element) {
+          element.style.transform = `translate3d(
+            calc(${position.x}px - 50%), calc(${position.y}px - 50%), 0)`
+        }
+      },
+    },
+  }))
+
+  const onDragStart: MouseEventHandler<HTMLDivElement> = (event) => {
+    event.currentTarget.style.cursor = 'grabbing'
+    isDragging.current = true
+  }
+  const onDragMove: MouseEventHandler<HTMLDivElement> = (event) => {
+    if (isDragging.current) {
+      const newPosition = {
+        x: event.clientX,
+        y: event.clientY,
+      }
+      set({ mousePosition: newPosition })
+    }
+  }
+  const onDragStop: MouseEventHandler<HTMLDivElement> = (event) => {
+    event.currentTarget.style.cursor = 'initial'
+    isDragging.current = false
+  }
+
+  return (
+    <div style={{ padding: '2em' }}>
+      <p>Drag the circle around or change its position from leva input.</p>
+      <Circle
+        ref={circleRef}
+        onPointerDown={onDragStart}
+        onPointerMove={onDragMove}
+        onPointerUp={onDragStop}
+        onPointerOut={onDragStop}
+      />
+    </div>
+  )
+}
+
+OnChangeAndSet.storyName = 'onChange and set'
