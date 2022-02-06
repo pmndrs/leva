72a6d668321f001e241d0ae7b22855a733396511
David Bismut <david.bismut@gmail.com>
2022-02-06 20:49:04 +0100

    story: storybook examples

--- packages/leva/stories/inputs/String.stories.tsx
+++ packages/leva/stories/inputs/String.stories.tsx
@@ -26,3 +26,15 @@ export const Simple = Template.bind({})
 Simple.args = {
   value: 'Leva is awesome',
 }
+
+export const Textarea = Template.bind({})
+Textarea.args = {
+  value: 'Leva also supports <textarea/>\nAllowing for\nmultiple\nlines',
+  textarea: true,
+}
+
+export const NonEditable = Template.bind({})
+NonEditable.args = {
+  value: 'This text is not editable but still supports\nline\nbreaks.',
+  editable: false,
+}
