cd20de681a188d95918dfe51febd8cbadb7526b2
David Bismut <david.bismut@gmail.com>
2022-02-07 06:46:01 +0100

    types: fix editable prop

--- packages/leva/src/components/String/String.tsx
+++ packages/leva/src/components/String/String.tsx
@@ -6,13 +6,13 @@ import type { StringProps } from './string-types'
 import { styled } from '../../styles'
 
 type BaseStringProps = Pick<StringProps, 'displayValue' | 'onUpdate' | 'onChange'> &
-  Omit<ValueInputProps, 'value'> & { editable: boolean }
+  Omit<ValueInputProps, 'value'> & { editable?: boolean }
 
 const NonEditableString = styled('div', {
   whiteSpace: 'pre-wrap',
 })
 
-export function String({ displayValue, onUpdate, onChange, editable, ...props }: BaseStringProps) {
+export function String({ displayValue, onUpdate, onChange, editable = true, ...props }: BaseStringProps) {
   if (editable) return <ValueInput value={displayValue} onUpdate={onUpdate} onChange={onChange} {...props} />
   return <NonEditableString>{displayValue}</NonEditableString>
 }
