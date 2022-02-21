b5b75b3ab44abfd51848fbb253aa2657ebc7d6aa
David Bismut <david.bismut@gmail.com>
2022-02-21 18:44:59 +0100

    fix: pass the `disabled` prop to the checkbox input

--- packages/leva/src/components/Boolean/Boolean.tsx
+++ packages/leva/src/components/Boolean/Boolean.tsx
@@ -4,10 +4,21 @@ import { Label, Row } from '../UI'
 import { StyledInputWrapper } from './StyledBoolean'
 import type { BooleanProps } from './boolean-types'
 
-export function Boolean({ value, onUpdate, id }: Pick<BooleanProps, 'value' | 'onUpdate' | 'id'>) {
+export function Boolean({
+  value,
+  onUpdate,
+  id,
+  disabled,
+}: Pick<BooleanProps, 'value' | 'onUpdate' | 'id' | 'disabled'>) {
   return (
     <StyledInputWrapper>
-      <input id={id} type="checkbox" checked={value} onChange={(e) => onUpdate(e.currentTarget.checked)} />
+      <input
+        id={id}
+        type="checkbox"
+        checked={value}
+        onChange={(e) => onUpdate(e.currentTarget.checked)}
+        disabled={disabled}
+      />
       <label htmlFor={id}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
@@ -18,12 +29,12 @@ export function Boolean({ value, onUpdate, id }: Pick<BooleanProps, 'value' | 'o
 }
 
 export function BooleanComponent() {
-  const { label, value, onUpdate, id } = useInputContext<BooleanProps>()
+  const { label, value, onUpdate, disabled, id } = useInputContext<BooleanProps>()
 
   return (
     <Row input>
       <Label>{label}</Label>
-      <Boolean value={value} onUpdate={onUpdate} id={id} />
+      <Boolean value={value} onUpdate={onUpdate} id={id} disabled={disabled} />
     </Row>
   )
 }
