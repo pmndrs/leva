e9121f78b210757bf82fd972c3589ce7dc4527f9
Gianmarco <gianmarcosimone89@gmail.com>
2025-10-31 17:51:42 +0100

    get rid of vitest stuff

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   stories: ['../packages/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-docs', '@storybook/addon-vitest'],
+  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {
