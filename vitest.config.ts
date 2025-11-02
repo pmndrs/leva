6d4d1347717e3256917df0c119388e7154ddeaf1
Gianmarco <gianmarcosimone89@gmail.com>
2025-11-03 00:14:50 +0100

    .

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
+    exclude: ['**/*.test.ts', '**/node_modules/**', '**/dist/**'],
   },
   resolve: {
     alias: {
